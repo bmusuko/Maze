1e303cb416f39d343bb08ddf30e74504fb5d3bcd
Bram Musuko Panjaitan <32943475+bmusuko@users.noreply.github.com>
2019-03-27 15:54:15 +0700

    Add files via upload

--- maze.py
+++ maze.py
@@ -33,7 +33,7 @@ def main():
 	#yawal = 0
 	#xakhir = 2
 	#yakhir = 4
-
+	telusurBFS = 0
 	print(xawal,yawal,xakhir,yakhir,n,m)
 	antrian = [(xawal,yawal,[(xawal,yawal)])]		
 	found = False
@@ -47,36 +47,44 @@ def main():
 			a[y][x] = 1
 			#print(x,y)
 			if(x == xakhir and y == yakhir):
-				jalurBFS = s	
+				jalurBFS = deepcopy(s)	
 				found = True
 			else:
 				if(y>0): 
 					if(a[y-1][x] != 1):
-
-						s.append((x-1,y))
-						antrian.append((x,y-1,s))
-						s.pop()
-						print("Down")
+						telusurBFS += 1
+						l = deepcopy(s)
+						l.append((x,y-1))
+						antrian.append((x,y-1,l))
+						#print("Down")
 				if(y<n-1): #top
 					if(a[y+1][x] != 1):
-						s.append((x+1,y))
-						antrian.append((x,y+1,s))
-						s.pop()
-						print("Up")
+						telusurBFS += 1
+						t = deepcopy(s)
+						t.append((x,y+1))
+						antrian.append((x,y+1,t))
+						#print("Up")
 				if(x<m-1): #right
 					if(a[y][x+1] != 1):
-						s.append((x,y+1))
-						antrian.append((x+1,y,s))
-						s.pop()
-						print("Right")
+						telusurBFS += 1
+						r = deepcopy(s)
+						r.append((x+1,y))
+						antrian.append((x+1,y,r))
+						#print("Right")
 				if(x>0): #left
 					if(a[y][x-1] != 1):
-						s.append((x,y-1))
-						antrian.append((x-1,y,s))
-						s.pop()
-						print("left")
+						telusurBFS += 1
+						le = deepcopy(s)
+						le.append((x-1,y))
+						antrian.append((x-1,y,le))
+						#print("left")
+	if(found):
+		print(jalurBFS)
+		print("penelusuran BFS sebanyak : ",telusurBFS)
 	found = False
-	antrian = [(xawal,yawal,jarak(xawal,yawal,xakhir,yakhir),[xawal,yawal])]
+	antrian = [(xawal,yawal,jarak(xawal,yawal,xakhir,yakhir),[(xawal,yawal)])]
+	
+	telusurAstar = 0
 	a = deepcopy(b)
 	while not found: # A*
 		#print(antrian)
@@ -88,35 +96,43 @@ def main():
 			a[y][x] = 1
 			#print(x,y)
 			if(x == xakhir and y == yakhir):
-				jalurBFS = s	
+				jalurAStar = deepcopy(s)
 				found = True
 			else:
-				if(y>0): 
+				if(y>0): #down 
 					if(a[y-1][x] != 1):
-						s.append((x-1,y))
-						antrian.append((x,y-1,f+jarak(x-1,y,xakhir,yakhir),s))
-						s.pop()
-						print("Down")
+						telusurAstar += 1
+						l = deepcopy(s)
+						l.append((x,y-1))
+						antrian.append((x,y-1,f+jarak(x-1,y,xakhir,yakhir),l))
+						#print("Down")
 				if(y<n-1): #top
 					if(a[y+1][x] != 1):
-						s.append((x+1,y))
-						antrian.append((x,y+1,f+jarak(x+1,y,xakhir,yakhir),s))
-						s.pop()
-						print("Up")
+						telusurAstar += 1
+						t = deepcopy(s)
+						t.append((x,y+1))
+						antrian.append((x,y+1,f+jarak(x+1,y,xakhir,yakhir),t))
+						#print("Up")
 				if(x<m-1): #right
 					if(a[y][x+1] != 1):
-						s.append((x,y+1))
-						antrian.append((x+1,y,f+jarak(x,y+1,xakhir,yakhir),s))
-						s.pop()
-						print("Right")
+						telusurAstar += 1
+						r = deepcopy(s)
+						r.append((x+1,y))
+						antrian.append((x+1,y,f+jarak(x,y+1,xakhir,yakhir),r))
+						#print("Right")
 				if(x>0): #left
 					if(a[y][x-1] != 1):
-						s.append((x,y-1))
-						antrian.append((x-1,y,f+jarak(x,y-1,xakhir,yakhir),s))
-						s.pop()
-						print("left")
+						telusurAstar += 1
+						le = deepcopy(s)
+						le.append((x-1,y))
+						antrian.append((x-1,y,f+jarak(x,y-1,xakhir,yakhir),le))
+						#print("left")
+
 		antrian = sorted(antrian,key=lambda x:x[2])
-		#print(antrian)
+		
+	if(found):
+			print(jalurAStar)
+			print("penelusuran A* sebanyak : ",telusurAstar)
 
 
 if __name__ == '__main__':
